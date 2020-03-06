16eed17d835aeecb02860de68bea86232b6722f8
João Sarmento <joaolrsarmento@gmail.com>
2020-03-06 13:54:42 -0300

    Created users, constants

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.db import models
+from django import forms
+
+class User:
+    username = models.CharField(unique=True, max_length=100)
+    password = forms.CharField(widget = forms.PasswordInput, max_length = 16)
+    email = models.EmailField(unique=True, max_length=255)
+    birth_date = models.DateTimeField(auto_now_add=True)
+
