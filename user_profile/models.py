7163b3e0415994a3d4ae25085d7a1b29c1d3c006
João Sarmento <joaolrsarmento@gmail.com>
2020-03-07 13:59:10 -0300

    better file configuration

--- user_profile/models.py
+++ user_profile/models.py
@@ -12,3 +12,16 @@ class User:
     email = models.EmailField(unique=True, max_length=255)
     birth_date = models.DateTimeField(auto_now_add=True)
 
+class AdvancedUser(User):
+    def __init__(self):
+        self.gross_monthly_income = None
+        self.investment_suggestion = None
+        self.level = None
+        self.extern_evaluation = None
+        self.social_class = None
+        self.region = None
+        self.person_profile = None
+        self.age = None
+        self.likes = None
+        self.month_save_suggestion = None
+        self.badges = []
