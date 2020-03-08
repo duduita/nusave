387aea96f03c472aeda9e638b2e75b82a6e9d659
Paulo Ricardo <prsfc.ubr@gmail.com>
2020-03-08 04:36:07 -0500

    statistical functions

--- data/example_creator.py
+++ data/example_creator.py
@@ -2,16 +2,25 @@ import pandas as pd
 import numpy as np
 import random as rd
 from random_generator import random_generator
+from statistics import statistics
 
 #--------------------------------------------------------------------
+
 contas = pd.read_csv('example.csv', index_col=0)
-print(contas)
 columns = ['Alimentacao', 'Assinatura e servicos','Educacao', 'Roupas', 'Saude'
            'Transporte','Outros','Saques']
-contas = random_generator(contas, columns)
-#--------------------------------------------------------------------
-print(contas)
-contas.to_json('contas_example.json')
+category = ['A', 'metropolitana', '50+', '4+ filhos', 'mulher']
+data = []
+for i in range(0,12):
+    data.append(contas)
+    random_generator(data[i], columns)
+
+print(data[1]['Alimentacao':'Saques']['Paulo'].sum())
+
+
+
+
+
 
 
 
--- data/statistics.py
+++ data/statistics.py
@@ -2,22 +2,99 @@ import pandas as pd
 import numpy as np
 
 
-def get_mean(filter, mes, data):
-    data_mes = data[data['Mes'] == mes]
-    return data_mes[filter].mean()
+class statistics():
+    month_list = ['janeiro', 'fevereiro', 'marco', 'abril',
+                  'maio', 'junho', 'julho', 'agosto',
+                  'setembro', 'outubro', 'novembro', 'dezembro']
+    columns = ['Alimentacao', 'Assinatura e servicos', 'Educacao',
+               'Roupas', 'Saude', 'Transporte', 'Outros', 'Saques']
 
+    def __init__(self, data, curr_month):
+        '''
 
-def get_quantile_85(filter, mes, data):
-    data_mes = data[data['Mes'] == mes]
-    return data_mes[filter].quantile(85)
+        :param data: array de DataFrames
+        :param curr_month: mes atual
+        '''
+        self.data = data
+        self.curr_index = statistics.month_list.index(curr_month)
+        if (self.curr_index > 0):
+            self.last_index = self.curr_index - 1
+        else:
+            self.last_index = 11
 
+    def getUserLastMonth(self, user, filter):
+        return self.data[self.last_index][filter][user]
 
-def get_quantile(filter, mes, data):
-    data_mes = data[data['Mes'] == mes]
-    return data_mes[filter].quantile(85)
+    def getUserAverage(self, user, filter):
+        media = 0
+        alfa = 1 / 6
+        i = self.last_index
 
+        while (i != self.curr_index):
+            media = alfa * self.data[i][filter][user] + (1 - alfa) * media
+            if (i > 0):
+                i = i - 1
+            else:
+                i = 11
+        return media
 
-def get_expend(filter, user, mes, data):
-    data_mes = data[data['Mes'] == mes]
-    return data_mes[user][filter]
+    def getCategoryAverage(self, category, filter):
+        '''
 
+        :param category: array de strings
+        as strings que definem a categoria devem estar na exata ordem: Classe,Regiao,Idade,Filhos,Sexo
+        :param filter: tipo de gasto
+        :return: media
+        '''
+        media = 0
+        alfa = 1 / 6
+        i = self.last_index
+
+        while (i != self.curr_index):
+            data_month = self.data[i]
+            data_category = data_month[(data_month['Classe'] == category[0])
+                                       & (data_month['Regiao'] == category[1])
+                                       & (data_month['Idade'] == category[2])
+                                       & (data_month['Filhos'] == category[3])
+                                       & (data_month['Sexo'] == category[4])
+                                       ]
+            media = alfa*data_category[filter].mean() + (1 - alfa)*media
+            if (i>0):
+                i = i - 1
+            else:
+                i = 11
+        return media
+
+    def getCategoryPercentiles(self, category, filter, quantile):
+        '''
+
+                :param category: array de strings
+                as strings que definem a categoria devem estar na exata ordem: Classe,Regiao,Idade,Filhos,Sexo
+                :param filter: tipo de gasto
+                :return: media
+                '''
+        media = 0
+        alfa = 1 / 6
+        i = self.last_index
+
+        while (i != self.curr_index):
+            data_month = self.data[i]
+            data_category = data_month[(data_month['Classe'] == category[0])
+                                       & (data_month['Regiao'] == category[1])
+                                       & (data_month['Idade'] == category[2])
+                                       & (data_month['Filhos'] == category[3])
+                                       & (data_month['Sexo'] == category[4])
+                                       ]
+            media = alfa * data_category[filter].quantile(quantile) + (1 - alfa) * media
+            if (i > 0):
+                i = i - 1
+            else:
+                i = 11
+        return media
+
+    def getUserLastMonthPercentage(self, user):
+        soma = 0
+        for filter in statistics.columns:
+            data_expends = self.data[self.last_index][filter]
+            soma = soma + data_expends[user]
+        return soma
