d38001f136dc791ef7cfa124407c0caf873cc42b
Paulo Ricardo <prsfc.ubr@gmail.com>
2020-03-07 19:33:49 -0600

    statistical functions

--- data/statistics.py
+++ data/statistics.py
@@ -1,25 +1,22 @@
 import pandas as pd
 import numpy as np
 
-def statistic_request(filter, user, data):
-   '''
 
-   :param filter:
-   :param data:
-   :return:
-   '''
-
-def mean_request(filter, data):
-    return data[filter].mean()
-
-def quantile15_request(filter, data):
-    return data[filter].quantile(0.15)
-
-def quantile85_request(filter, data):
-    return data[filter].quantile(0.85)
-
-def filter_resquest(filter, user, data):
-    return data[user][filter]
+def get_mean(filter, mes, data):
+    data_mes = data[data['Mes'] == mes]
+    return data_mes[filter].mean()
 
 
+def get_quantile_85(filter, mes, data):
+    data_mes = data[data['Mes'] == mes]
+    return data_mes[filter].quantile(85)
 
+
+def get_quantile(filter, mes, data):
+    data_mes = data[data['Mes'] == mes]
+    return data_mes[filter].quantile(85)
+
+
+def get_expend(filter, user, mes, data):
+    data_mes = data[data['Mes'] == mes]
+    return data_mes[user][filter]
